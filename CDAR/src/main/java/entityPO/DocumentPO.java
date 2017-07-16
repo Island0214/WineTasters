d5c3a64c7c43c6175ef43deb51aebef5940199bc
Nirvana1997 <408985936@qq.com>
2017-07-16 12:11:18 +0800

    feat：DocumentPO，添加关键词

--- CDAR/src/main/java/entityPO/DocumentPO.java
+++ CDAR/src/main/java/entityPO/DocumentPO.java
@@ -20,21 +20,22 @@ public class DocumentPO {
     private String caseNumber;
     //全文、文首、案件性质
     private String property;
-    //全文、诉讼记录、案由
+    //全文、诉讼记录、案由（nullable）
     private String reason;
     //全文、文首、审判程序
     private String process;
     //全文、文尾、裁判时间
     private Date endDate;
-    //全文、诉讼参与人全集、（被告）
-    private String defendant;
-    //全文、诉讼参与人全集、（原告）
-    private String plaintiff;
-    //全文、案件基本情况
+    //全文、诉讼参与人全集、（被告、被告）
+    //当事人
+    private String litigant;
+    //全文、案件基本情况（nullable）
     private String situation;
     //全文、裁判分析过程、法律法条名称
     //形如"《中华人民共和国侵权责任法》:第六条、第十六条/《最高人民法院关于确定民事侵权精神损害赔偿责任若干问题的解释》:第八条"
     private String evidence;
+    //关键词，以/分割
+    private String keywords;
 
     @Id
     @GeneratedValue
@@ -87,22 +88,6 @@ public class DocumentPO {
         this.endDate = endDate;
     }
 
-    public String getDefendant() {
-        return defendant;
-    }
-
-    public void setDefendant(String defendant) {
-        this.defendant = defendant;
-    }
-
-    public String getPlaintiff() {
-        return plaintiff;
-    }
-
-    public void setPlaintiff(String plaintiff) {
-        this.plaintiff = plaintiff;
-    }
-
     public String getCaseNumber() {
         return caseNumber;
     }
@@ -134,5 +119,21 @@ public class DocumentPO {
 
     public void setSituation(String situation) {
         this.situation = situation;
+    }
+
+    public String getLitigant() {
+        return litigant;
+    }
+
+    public void setLitigant(String litigant) {
+        this.litigant = litigant;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
     }
 }
