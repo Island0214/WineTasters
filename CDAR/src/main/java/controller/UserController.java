14022973d4288f84c9f2bb9075c8733da7959ff3
Island0214 <549378813@qq.com>
2017-07-17 12:06:29 +0800

    <feat> <ui> : <主界面增加用户登陆注册框>

---
+++ CDAR/src/main/java/controller/UserController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by island on 2017/7/17.
+ */
+@Controller
+@RequestMapping("/userAuction")
+public class UserController {
+
+}
