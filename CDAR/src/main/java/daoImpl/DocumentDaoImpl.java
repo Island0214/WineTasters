d7873dc87f87b523020723666afea373245be0b8
151250175 <151250175@smail.nju.edu.cn>
2017-07-20 10:29:39 +0800

    change:修改推荐案例数量为5

--- CDAR/src/main/java/daoImpl/DocumentDaoImpl.java
+++ CDAR/src/main/java/daoImpl/DocumentDaoImpl.java
@@ -95,7 +95,7 @@ public class DocumentDaoImpl implements DocumentDao{
             q.setParameter(i,"%"+keywords.get(i)+"%");
         }
 
-        q.setMaxResults(10);
+        q.setMaxResults(5);
 
         List<DocumentPO> result = q.getResultList();
         session.close();
