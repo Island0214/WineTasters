f0aa08881556b3980a19d4e5d63980912b102320
Nirvana1997 <408985936@qq.com>
2017-07-26 16:49:04 +0800

    修改vo名字

--- CDAR/src/main/java/entityVO/DocumentVO.java
+++ CDAR/src/main/java/entityVO/DocumentVO.java
@@ -10,7 +10,7 @@ public class DocumentVO {
      * caseNumber：案号
      * court：审理法院
      * endDate：裁判日期
-     * judge_reason:裁判理由，开头是“本院认为。。。”那一段
+     * judgeReason:裁判理由，开头是“本院认为。。。”那一段
      *
      * 具体案例页需要的提取结果概要：
      * property：案件类型
@@ -32,7 +32,7 @@ public class DocumentVO {
     //裁判日期
     public String endDate;
     //裁判理由
-    public String judge_reason;
+    public String judgeReason;
 
     //案件类型
     public String property;
@@ -56,7 +56,7 @@ public class DocumentVO {
      * @param caseNumber 案号
      * @param court 审理法院
      * @param endDate 裁判日期
-     * @param judge_reason 裁判理由
+     * @param judgeReason 裁判理由
      * @param property 案件类型
      * @param evidence 法律依据
      * @param process 审理程序
@@ -65,14 +65,14 @@ public class DocumentVO {
      * @param originDocument 裁判书原文
      */
     public DocumentVO(Integer id, String title, String caseNumber, String court, String endDate,
-                      String judge_reason, String property, String evidence, String process,
+                      String judgeReason, String property, String evidence, String process,
                       String reason, String litigant, String originDocument, String keywords) {
         this.id = id;
         this.title = title;
         this.caseNumber = caseNumber;
         this.court = court;
         this.endDate = endDate;
-        this.judge_reason = judge_reason;
+        this.judgeReason = judgeReason;
         this.property = property;
         this.evidence = evidence;
         this.process = process;
